2aa42bfa381d4915320a1edcf35e9e33eed0d324
Chris Hagan <chris@stackableregiments.com>
2016-04-01 11:54:10 +1100

    building up yaws launch with mounted docroot

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+from isqad88/erl-yaws
+MAINTAINER Chris Hagan<chris@stackableregiments.com>
+
+CMD ["/usr/local/bin/yaws","--conf","/srv/yaws/conf"
---
+++ viewer.sh
@@ -0,0 +1,1 @@
+docker run -v ~/Desktop/Tsung/logs:/srv/logs -v ~/Desktop/Viewer/conf:/srv/yaws/conf -v ~/Desktop/Viewer/www:/srv/yaws/www -it viewer
